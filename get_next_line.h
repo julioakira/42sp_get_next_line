04ed14b24d59426a62b7dc68f6df5ac872305780
julioakira <julio.akira.pedroso@gmail.com>
2021-08-24 23:22:26 -0300

    first commit - adds basic files

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jakira-p <jakira-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/24 22:58:15 by jakira-p          #+#    #+#             */
+/*   Updated: 2021/08/24 23:21:53 by jakira-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+
+char	*get_next_line(int fd);
+
+#endif
